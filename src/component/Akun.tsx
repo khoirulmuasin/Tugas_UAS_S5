c099c9db31d62f2c4bdc05a5b9ed5c0d86882d76
khoirulmuasin <gabutcrypto1@gmail.com>
2023-12-21 06:29:24 +0700

    membuat halaman produk

--- src/component/Akun.tsx
+++ src/component/Akun.tsx
@@ -1,12 +1,236 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { Image, Text, View, ScrollView, StatusBar } from 'react-native'
+import React, { Component } from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
 
-const Akun = () => {
-    return (
-        <View>
-            <Text>Akun</Text>
-        </View>
-    )
+
+export class App extends Component {
+    render() {
+        return (
+            <ScrollView style={{ flex: 1, backgroundColor: '#1E272E' }}>
+                <StatusBar barStyle='light-content' backgroundColor={'#1E272E'} />
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    paddingTop: 5,
+                    paddingEnd: 1,
+                }}>
+                    <Icon name="menu-outline" size={50} color="#FAD390" />
+                    <Icon name="notifications-outline" size={50} color="#FAD390" />
+                </View>
+                <Text style={{
+                    color: '#FAD390',
+                    padding: 5,
+                    paddingTop: 2,
+                    fontSize: 20,
+                }}>HI, Bosss</Text>
+                <View style={{
+                    backgroundColor: '#FAD390',
+                    width: 382,
+                    height: 102,
+                    borderRadius: 6,
+                    marginLeft: 5,
+                    marginTop: 27,
+                    flexDirection: 'row',
+                }}>
+                    <Image style={{
+                        width: 143, height: 103,
+                    }}
+                        source={require('../image/5.png')} />
+                    <View>
+                        <Text style={{
+                            fontSize: 15,
+                            color: '#000000',
+                            fontWeight: 'bold',
+                            marginLeft: 80,
+                            marginTop: 19,
+                        }}>Selamat Mencoba</Text>
+                        <Text style={{
+                            fontSize: 15,
+                            color: '#000000',
+                            fontWeight: 'bold',
+                            marginLeft: 80,
+                        }}>Produc Kami</Text>
+                        <Text style={{
+                            fontSize: 17,
+                            color: '#000000',
+                            fontWeight: 'bold',
+                            marginLeft: 80,
+                        }}>Diskon 80%</Text>
+                    </View>
+                </View>
+                <Text style={{
+                    color: '#FAD390',
+                    fontSize: 20,
+                    marginLeft: 5,
+                    marginTop: 25,
+                }}>Categories</Text>
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    marginTop: 24,
+                    marginEnd: 19,
+                    marginLeft: 19,
+                }}>
+                    <Text style={{ color: '#FAD390', fontSize: 15, fontWeight: 'bold' }}>Harga 40k</Text>
+                    <Text style={{ color: '#FAD390', fontSize: 15, }}>Harga 50k</Text>
+                    <Text style={{ color: '#FAD390', fontSize: 15, }}>Harga 60k</Text>
+                    <Text style={{ color: '#FAD390', fontSize: 15, }}>Harga 70k</Text>
+                </View>
+                <View style={{
+                    flexDirection: 'row', justifyContent: 'space-between',
+                    marginLeft: 19,
+                    marginEnd: 19,
+                }}>
+                    <View style={{
+                        backgroundColor: '#FF9F1A',
+                        width: 175,
+                        height: 209,
+                        borderRadius: 6,
+                        marginLeft: 5,
+                        marginTop: 27,
+                    }}>
+                        <Image style={{
+                            width: 105,
+                            height: 100,
+                            marginLeft: 25,
+                            marginTop: 40,
+                        }} source={require('../image/1.png')} />
+                        <View style={{
+                            backgroundColor: '#FAD390',
+                            width: 163,
+                            height: 33,
+                            borderRadius: 6,
+                            marginLeft: 5,
+                            marginTop: 25,
+                        }}>
+                            <Text style={{
+                                fontSize: 12,
+                                color: '#000000',
+                                fontWeight: 'bold',
+                                marginLeft: 6,
+                            }}>Tas Bagus</Text>
+                            <Text style={{
+                                fontSize: 10,
+                                color: '#000000',
+                                marginLeft: 6,
+                            }}>Rp. 200k</Text>
+                        </View>
+                    </View>
+                    <View style={{
+                        backgroundColor: '#FF9F1A',
+                        width: 175,
+                        height: 209,
+                        borderRadius: 6,
+                        marginLeft: 5,
+                        marginTop: 27,
+                    }}>
+                        <Image style={{
+                            width: 105,
+                            height: 100,
+                            marginLeft: 25,
+                            marginTop: 40,
+                        }} source={require('../image/2.png')} />
+                        <View style={{
+                            backgroundColor: '#FAD390',
+                            width: 163,
+                            height: 33,
+                            borderRadius: 6,
+                            marginLeft: 5,
+                            marginTop: 25,
+                        }}>
+                            <Text style={{
+                                fontSize: 12,
+                                color: '#000000',
+                                fontWeight: 'bold',
+                                marginLeft: 6,
+                            }}>Tas Bagus</Text>
+                            <Text style={{
+                                fontSize: 10,
+                                color: '#000000',
+                                marginLeft: 6,
+                            }}>Rp. 200k</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={{
+                    flexDirection: 'row', justifyContent: 'space-between',
+                    marginLeft: 19,
+                    marginEnd: 19,
+                }}>
+                    <View style={{
+                        backgroundColor: '#FF9F1A',
+                        width: 175,
+                        height: 209,
+                        borderRadius: 6,
+                        marginLeft: 5,
+                        marginTop: 27,
+                    }}>
+                        <Image style={{
+                            width: 105,
+                            height: 100,
+                            marginLeft: 25,
+                            marginTop: 40,
+                        }} source={require('../image/3.png')} />
+                        <View style={{
+                            backgroundColor: '#FAD390',
+                            width: 163,
+                            height: 33,
+                            borderRadius: 6,
+                            marginLeft: 5,
+                            marginTop: 25,
+                        }}>
+                            <Text style={{
+                                fontSize: 12,
+                                color: '#000000',
+                                fontWeight: 'bold',
+                                marginLeft: 6,
+                            }}>Tas Bagus</Text>
+                            <Text style={{
+                                fontSize: 10,
+                                color: '#000000',
+                                marginLeft: 6,
+                            }}>Rp. 200k</Text>
+                        </View>
+                    </View>
+                    <View style={{
+                        backgroundColor: '#FF9F1A',
+                        width: 175,
+                        height: 209,
+                        borderRadius: 6,
+                        marginLeft: 5,
+                        marginTop: 27,
+                    }}>
+                        <Image style={{
+                            width: 105,
+                            height: 100,
+                            marginLeft: 25,
+                            marginTop: 40,
+                        }} source={require('../image/4.png')} />
+                        <View style={{
+                            backgroundColor: '#FAD390',
+                            width: 163,
+                            height: 33,
+                            borderRadius: 6,
+                            marginLeft: 5,
+                            marginTop: 25,
+                        }}>
+                            <Text style={{
+                                fontSize: 12,
+                                color: '#000000',
+                                fontWeight: 'bold',
+                                marginLeft: 6,
+                            }}>Tas Bagus</Text>
+                            <Text style={{
+                                fontSize: 10,
+                                color: '#000000',
+                                marginLeft: 6,
+                            }}>Rp. 200k</Text>
+                        </View>
+                    </View>
+                </View>
+            </ScrollView >
+        )
+    }
 }
 
-export default Akun
+export default App
--- src/component/Produk.tsx
+++ src/component/Produk.tsx
@@ -1,235 +1,114 @@
-import { Image, Text, View, ScrollView } from 'react-native'
-import React, { Component } from 'react'
+import React, { useRef } from 'react';
+import { ScrollView, View, Image, Text, TextInput, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+const data = [
+    { id: '1', image: require('../image/1.png'), name: 'Tas Bagus', price: 'Rp. 200k' },
+    { id: '2', image: require('../image/3.png'), name: 'Tas Bagus', price: 'Rp. 200k' },
+    { id: '3', image: require('../image/4.png'), name: 'Tas Bagus', price: 'Rp. 200k' },
+    { id: '4', image: require('../image/1.png'), name: 'Tas Bagus', price: 'Rp. 200k' },
+    { id: '4', image: require('../image/3.png'), name: 'Tas Bagus', price: 'Rp. 200k' },
+    { id: '4', image: require('../image/4.png'), name: 'Tas Bagus', price: 'Rp. 200k' },
+    { id: '4', image: require('../image/1.png'), name: 'Tas Bagus', price: 'Rp. 200k' },
+    { id: '4', image: require('../image/3.png'), name: 'Tas Bagus', price: 'Rp. 200k' },
+    { id: '4', image: require('../image/1.png'), name: 'Tas Bagus', price: 'Rp. 200k' },
+];
 
-export class App extends Component {
-    render() {
-        return (
-            <ScrollView style={{ flex: 1, backgroundColor: '#1E272E' }}>
-                <View style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    paddingTop: 5,
-                    paddingEnd: 1,
-                }}>
-                    <Icon name="menu-outline" size={50} color="#FAD390" />
-                    <Icon name="notifications-outline" size={50} color="#FAD390" />
-                </View>
-                <Text style={{
-                    color: '#FAD390',
-                    padding: 5,
-                    paddingTop: 2,
-                    fontSize: 20,
-                }}>HI, Bosss</Text>
-                <View style={{
-                    backgroundColor: '#FAD390',
-                    width: 382,
-                    height: 102,
-                    borderRadius: 6,
-                    marginLeft: 5,
-                    marginTop: 27,
-                    flexDirection: 'row',
-                }}>
-                    <Image style={{
-                        width: 143, height: 103,
-                    }}
-                        source={require('../image/5.png')} />
-                    <View>
-                        <Text style={{
-                            fontSize: 15,
-                            color: '#000000',
-                            fontWeight: 'bold',
-                            marginLeft: 80,
-                            marginTop: 19,
-                        }}>Selamat Mencoba</Text>
-                        <Text style={{
-                            fontSize: 15,
-                            color: '#000000',
-                            fontWeight: 'bold',
-                            marginLeft: 80,
-                        }}>Produc Kami</Text>
-                        <Text style={{
-                            fontSize: 17,
-                            color: '#000000',
-                            fontWeight: 'bold',
-                            marginLeft: 80,
-                        }}>Diskon 80%</Text>
-                    </View>
-                </View>
-                <Text style={{
-                    color: '#FAD390',
-                    fontSize: 20,
-                    marginLeft: 5,
-                    marginTop: 25,
-                }}>Categories</Text>
-                <View style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    marginTop: 24,
-                    marginEnd: 19,
-                    marginLeft: 19,
-                }}>
-                    <Text style={{ color: '#FAD390', fontSize: 15, fontWeight: 'bold' }}>Harga 40k</Text>
-                    <Text style={{ color: '#FAD390', fontSize: 15, }}>Harga 50k</Text>
-                    <Text style={{ color: '#FAD390', fontSize: 15, }}>Harga 60k</Text>
-                    <Text style={{ color: '#FAD390', fontSize: 15, }}>Harga 70k</Text>
-                </View>
-                <View style={{
-                    flexDirection: 'row', justifyContent: 'space-between',
-                    marginLeft: 19,
-                    marginEnd: 19,
-                }}>
-                    <View style={{
-                        backgroundColor: '#FF9F1A',
-                        width: 175,
-                        height: 209,
-                        borderRadius: 6,
-                        marginLeft: 5,
-                        marginTop: 27,
-                    }}>
-                        <Image style={{
-                            width: 105,
-                            height: 100,
-                            marginLeft: 25,
-                            marginTop: 40,
-                        }} source={require('../image/5.png')} />
-                        <View style={{
-                            backgroundColor: '#FAD390',
-                            width: 163,
-                            height: 33,
-                            borderRadius: 6,
-                            marginLeft: 5,
-                            marginTop: 25,
-                        }}>
-                            <Text style={{
-                                fontSize: 12,
-                                color: '#000000',
-                                fontWeight: 'bold',
-                                marginLeft: 6,
-                            }}>Tas Bagus</Text>
-                            <Text style={{
-                                fontSize: 10,
-                                color: '#000000',
-                                marginLeft: 6,
-                            }}>Rp. 200k</Text>
-                        </View>
-                    </View>
-                    <View style={{
-                        backgroundColor: '#FF9F1A',
-                        width: 175,
-                        height: 209,
-                        borderRadius: 6,
-                        marginLeft: 5,
-                        marginTop: 27,
-                    }}>
-                        <Image style={{
-                            width: 105,
-                            height: 100,
-                            marginLeft: 25,
-                            marginTop: 40,
-                        }} source={require('../image/5.png')} />
-                        <View style={{
-                            backgroundColor: '#FAD390',
-                            width: 163,
-                            height: 33,
-                            borderRadius: 6,
-                            marginLeft: 5,
-                            marginTop: 25,
-                        }}>
-                            <Text style={{
-                                fontSize: 12,
-                                color: '#000000',
-                                fontWeight: 'bold',
-                                marginLeft: 6,
-                            }}>Tas Bagus</Text>
-                            <Text style={{
-                                fontSize: 10,
-                                color: '#000000',
-                                marginLeft: 6,
-                            }}>Rp. 200k</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={{
-                    flexDirection: 'row', justifyContent: 'space-between',
-                    marginLeft: 19,
-                    marginEnd: 19,
-                }}>
-                    <View style={{
-                        backgroundColor: '#FF9F1A',
-                        width: 175,
-                        height: 209,
-                        borderRadius: 6,
-                        marginLeft: 5,
-                        marginTop: 27,
-                    }}>
-                        <Image style={{
-                            width: 105,
-                            height: 100,
-                            marginLeft: 25,
-                            marginTop: 40,
-                        }} source={require('../image/5.png')} />
-                        <View style={{
-                            backgroundColor: '#FAD390',
-                            width: 163,
-                            height: 33,
-                            borderRadius: 6,
-                            marginLeft: 5,
-                            marginTop: 25,
-                        }}>
-                            <Text style={{
-                                fontSize: 12,
-                                color: '#000000',
-                                fontWeight: 'bold',
-                                marginLeft: 6,
-                            }}>Tas Bagus</Text>
-                            <Text style={{
-                                fontSize: 10,
-                                color: '#000000',
-                                marginLeft: 6,
-                            }}>Rp. 200k</Text>
-                        </View>
-                    </View>
-                    <View style={{
-                        backgroundColor: '#FF9F1A',
-                        width: 175,
-                        height: 209,
-                        borderRadius: 6,
-                        marginLeft: 5,
-                        marginTop: 27,
-                    }}>
-                        <Image style={{
-                            width: 105,
-                            height: 100,
-                            marginLeft: 25,
-                            marginTop: 40,
-                        }} source={require('../image/5.png')} />
-                        <View style={{
-                            backgroundColor: '#FAD390',
-                            width: 163,
-                            height: 33,
-                            borderRadius: 6,
-                            marginLeft: 5,
-                            marginTop: 25,
-                        }}>
-                            <Text style={{
-                                fontSize: 12,
-                                color: '#000000',
-                                fontWeight: 'bold',
-                                marginLeft: 6,
-                            }}>Tas Bagus</Text>
-                            <Text style={{
-                                fontSize: 10,
-                                color: '#000000',
-                                marginLeft: 6,
-                            }}>Rp. 200k</Text>
-                        </View>
-                    </View>
-                </View>
-            </ScrollView >
-        )
-    }
-}
+const Item = ({ item, isLeft }) => (
+    <View style={{
+        backgroundColor: '#FF9F1A',
+        width: 175,
+        height: 209,
+        borderRadius: 6,
+        margin: 5,
+        marginTop: 27,
+        marginLeft: isLeft ? 19 : 5,
+        marginRight: isLeft ? 5 : 19,
+    }}>
+        <Image style={{
+            width: 105,
+            height: 100,
+            marginLeft: 25,
+            marginTop: 40,
+        }} source={item.image} />
+        <View style={{
+            backgroundColor: '#FAD390',
+            width: 163,
+            height: 33,
+            borderRadius: 6,
+            marginLeft: 5,
+            marginTop: 25,
+        }}>
+            <Text style={{
+                fontSize: 12,
+                color: '#000000',
+                fontWeight: 'bold',
+                marginLeft: 6,
+            }}>{item.name}</Text>
+            <Text style={{
+                fontSize: 10,
+                color: '#000000',
+                marginLeft: 6,
+            }}>{item.price}</Text>
+        </View>
+    </View>
+);
 
-export default App
+const Akun = () => {
+    const scrollViewRef = useRef(null);
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.searchContainer}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder="Cari"
+                    placeholderTextColor="#888"
+                />
+                <Icon name="search" size={20} color="#888" style={styles.searchIcon} />
+            </View>
+            <ScrollView
+                ref={scrollViewRef}
+                contentContainerStyle={styles.scrollViewContent}
+                showsVerticalScrollIndicator={false}
+            >
+                <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+                    {data.map((item, index) => (
+                        <Item key={item.id} item={item} isLeft={index % 2 === 0} />
+                    ))}
+                </View>
+            </ScrollView>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#1E272E',
+    },
+    searchContainer: {
+        backgroundColor: '#FAD390',
+        paddingHorizontal: 10,
+        borderRadius: 7,
+        elevation: 2,
+        marginTop: 35,
+        marginHorizontal: 19,
+        marginBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    searchInput: {
+        flex: 1,
+        color: '#FAD390',  // Warna teks input
+    },
+    searchIcon: {
+        marginRight: 10,
+    },
+    scrollViewContent: {
+        marginLeft: 5,
+        marginRight: 5,
+    },
+});
+
+export default Akun;
